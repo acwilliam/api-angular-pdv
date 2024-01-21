8aed58da92cebf97de7c5530e61898b4ea11ebaf
acwilliammsn <thelordl2@hotmail.com>
2024-01-21 15:54:21 -0300

    feat: add component matToolbarModule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()]
+  providers: [provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    ]
 };
